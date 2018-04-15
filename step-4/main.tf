ef066a2c3b2923ab2e804cbf592b95f6cf2d26a5
Maxence Maireaux <maxence@realstorm.fr>
2018-04-15 18:18:05 +0200

    Add health_check in aws_lb_target_group

--- step-4/main.tf
+++ step-4/main.tf
@@ -53,6 +53,14 @@ resource "aws_lb_target_group" "test" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = "${data.aws_vpc.devoxx_vpc.id}"
+
+  health_check {
+    protocol            = "TCP"
+    port                = 80
+    healthy_threshold   = 3
+    unhealthy_threshold = 3
+    interval            = 10
+  }
 }
 
 resource "aws_lb_listener" "front_end" {
