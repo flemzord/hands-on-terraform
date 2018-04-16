e20f180483ca6dbfe4a337a7fdfe4a2ca40f2133
Aurélien Maury <cto@wescale.fr>
2018-04-16 15:11:04 +0200

    moins step4

--- step-4/main.tf
+++ step-4/main.tf
@@ -6,17 +6,3 @@
 #   https://www.terraform.io/docs/providers/aws/r/s3_bucket.html
 #
 
-resource "aws_s3_bucket" "devoxx-bucket" {
-  bucket = "${var.name}-bucket"
-  acl    = "private"
-
-  versioning {
-    enabled = true
-  }
-
-  tags {
-    Name = "${var.name}-bucket"
-  }
-
-  force_destroy = true
-}
