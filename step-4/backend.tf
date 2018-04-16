a19b0c184ba6735fc437277c152ae5d50062d8e9
Maxence Maireaux <maxence@realstorm.fr>
2018-04-16 10:21:01 +0200

    Fix 4_2

--- step-4/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "devoxx_bucket"
-    key    = "tfstate/"
-    region = "eu-west-3"
-  }
-}
--- step-4_2/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "lb_address" {
-  value = "${aws_lb.test.dns_name}"
-}
