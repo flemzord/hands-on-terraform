1cf83c6301c519edf1159ed35d0bbf5487afb307
Aurélien Maury <cto@wescale.fr>
2018-04-16 14:33:15 +0200

    redac step5 ok

--- step-5/backend.tf
+++ step-5/backend.tf
@@ -1,3 +1,8 @@
+#
+# TODO: Créer une configuration de backend de type s3 qui enregistre vos tfstates dans le bucket créé
+# en step-4.
+#
+#
 terraform {
   backend "s3" {
     bucket = "devoxx-bucket"
